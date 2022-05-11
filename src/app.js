3f7609b5a7ee8174e4c0f405aabecf54bcec8dff
JDigar <jdigar96@gmail.com>
2022-05-11 19:18:49 +0000

    finished OK

--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ window.onload = function() {
   for (let i = 0; i < 2; i++) {
     for (let x = 0; x < 2; x++) {
       for (let b = 0; b < 2; b++) {
-        console.log(pronoun[i], adj[x], noun[b] + ".com");
+        console.log(pronoun[i] + adj[x] + noun[b] + ".com");
       }
     }
   }
