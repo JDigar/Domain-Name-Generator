2ab74d0a3d2c36b0429779e66e8cd0cdab218e20
JDigar <jdigar96@gmail.com>
2022-05-11 19:12:03 +0000

    finished

--- src/app.js
+++ src/app.js
@@ -6,6 +6,17 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  for (let i = 0; i < 2; i++) {
+    for (let x = 0; x < 2; x++) {
+      for (let b = 0; b < 2; b++) {
+        console.log(pronoun[i], adj[x], noun[b] + ".com");
+      }
+    }
+  }
+
   console.log("Hello Rigo from the console!");
 };
